7b4a83192ee6ace66cfe2ed462232001a6bfe9d8
DuongKien <ddk0705@gmail.com>
2019-02-10 20:05:38 +0700

    Create module

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import { Component } from 'react';
+export default class LinkedInLogin extends Component<{}> {
+    render(): JSX.Element;
+}
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+    WebView,
+    TouchableOpacity,
+    View,
+    ViewPropTypes,
+    Text,
+    Modal,
+    StyleSheet,
+    Image,
+} from 'react-native';
+
+export default class LinkedInLogin extends Component<{}> {
+
+    render() {
+        return (
+            <View>
+                <Text>Linked In Login</Text>
+            </View>
+        );
+    }
+
+}
